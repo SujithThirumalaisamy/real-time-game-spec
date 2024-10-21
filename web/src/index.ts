d2fe4ae3657bfd9808ec727f8f04158013b8d8a4
Sargam <sargampoudel100@gmail.com>
2024-10-21 22:33:05 +0530

    feat: add routes for web server

--- web/src/index.ts
+++ web/src/index.ts
@@ -8,9 +8,16 @@ app.get("/", (req, res) => {
 });
 
 // POST request to /signup
+app.post("/signup", (req, res) => {});
+
 // POST request to /rooms with bearer token
+app.post("/rooms", (req, res) => {});
+
 // GET request to /users/userid
+app.get("/users/:userId", (req, res) => {});
+
 // PUT request to /users/userid/avatar
+app.get("/users/:userId/avatar", (req, res) => {});
 
 app.listen(3000, () => {
   console.log("Web Server Running On Port: 3000");
