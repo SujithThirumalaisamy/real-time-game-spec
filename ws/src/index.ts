f4c9c2b8dd44dc31ba8f7edcbbdc0aea5b636a5b
Sargam <sargampoudel100@gmail.com>
2024-10-21 22:25:35 +0530

    intit websocket

--- ws/src/index.ts
+++ ws/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Sargam WS!");
+import WebSocket from "ws";
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  console.log("Client Connected");
+
+  ws.on("message", (message) => {
+    console.log("Received message:", message.toString());
+
+    ws.send("Message received successfully!");
+  });
+
+  ws.on("close", () => {
+    console.log("Client Disconnected");
+  });
+});
